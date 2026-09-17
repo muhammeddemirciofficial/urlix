3c730889f5a272cff5a33763ce485c71a08077a7
muhammeddemirciofficial <demirci.muhammedd@gmail.com>
2026-09-17 13:27:40 +0300

    add OpenAPI and Swagger UI handlers

---
+++ internal/handler/docs.go
@@ -0,0 +1,43 @@
+package handler
+
+import (
+	"net/http"
+	"os"
+)
+
+func OpenAPI(w http.ResponseWriter, r *http.Request) {
+	content, err := os.ReadFile("/app/openapi.yaml")
+	if err != nil {
+		http.Error(w, "OpenAPI specification unavailable", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/yaml; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	_, _ = w.Write(content)
+}
+
+func Docs(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	_, _ = w.Write([]byte(`<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>URLIX API Docs</title>
+  <link rel="stylesheet" href="https://unpkg.com/swagger-ui-dist@5/swagger-ui.css">
+</head>
+<body>
+  <div id="swagger-ui"></div>
+  <script src="https://unpkg.com/swagger-ui-dist@5/swagger-ui-bundle.js"></script>
+  <script>
+    window.onload = () => {
+      window.ui = SwaggerUIBundle({
+        url: '/openapi.yaml',
+        dom_id: '#swagger-ui'
+      });
+    };
+  </script>
+</body>
+</html>`))
+}
